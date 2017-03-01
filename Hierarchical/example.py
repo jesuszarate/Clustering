465e9a14d1867819193834a785f0c974d5fa964f
Jesus <chuy8jay@gmail.com>
2017-02-28 21:38:29 -0700

    Finished implementation of Gonzales alg

--- Hierarchical/example.py
+++ Hierarchical/example.py
@@ -1,5 +1,5 @@
 import Queue as Q
-import heapq
+from matplotlib import pyplot
 
 import math
 
@@ -12,15 +12,9 @@ class pnt:
         self.id = id
         self.x = x
         self.y = y
-        #self.dist = dist
 
     def __str__(self):
         return "(" + self.x + "," + self.y + ")"
-
-    # def __cmp__(self, other):
-    #     return cmp(self.dist, other.dist)
-    # def __str__(self):
-    #     return "(" + self.id + ", " + str(self.dist) + ")"
 
 class distPoint:
     def __init__(self, dist, pnt):
@@ -38,6 +32,10 @@ class cluster:
         self.distances = {}
         self.points = set()
 
+x = []
+y = []
+z = []
+
 #read Files
 def Init2DClusters(path):
     cluster_list = []
@@ -45,10 +43,13 @@ def Init2DClusters(path):
         for line in f:
             l = line.split()
             n = pnt(l[0], l[1], l[2])
+            x.append(round(float(l[1]), 3))
+            y.append(round(float(l[2]), 3))
+            z.append(int(l[0]))
             cluster_list.append(n)
     return cluster_list
 
-clusters = Init2DClusters("/Users/jesuszarate/SchoolSemesters/Spring2017/CS6140-DataMining/Clustering/Resources/Ce.txt")
+clusters = Init2DClusters("/Users/jesuszarate/SchoolSemesters/Spring2017/CS6140-DataMining/Clustering/Resources/C1.txt")
 
 def distance(x1, x2, y1, y2):
     return math.sqrt((float(x2) - float(x1))**2 + (float(y2) - float(y1))**2)
@@ -56,54 +57,92 @@ def distance(x1, x2, y1, y2):
 distances = {}
 def Ds():
     for c1 in clusters:
-        print "ID: " + c1.id + "\t"
+        #print "ID: " + c1.id + "\t"
         clust = cluster(c1.id)
         clust.points.add(c1)
         distances[c1.id] = clust#set()
 
         for c2 in clusters:
             if c1 == c2:
-                print "X\t",
+                pass
+                #print "X\t",
             else:
                 dist = distance(c1.x, c2.x, c1.y, c2.y)
-                dp = distPoint(dist, c2)
+                dp = dist#distPoint(dist, c2)
                 distances[c1.id].distances[c2.id] = dp
-                print str(dist) + "\t",
+                #print str(dist) + "\t",
 
         print
 
+# First compute the distances
 Ds()
 
+# def updateDistance(id1, id2):
+#     for d in distances.values():
+#         if id1 in d.distances:
+#             d.distances.pop(id1)
+#         if id2 in d.distances:
+#             d.distances.pop(id2)
 
-def updateDistances(newClust, distances):
-
+def updateDistances(point1, point2, newClust, distances):
     for dp in distances.values():
-        min = distPoint(float("inf"), None)
-        for point in newClust.points:
-            if point.id in dp.distances:
-                d = dp.distances[point.id]
-                if min > d:
-                    min = dp.distances[point.id]
-                dp.distances.pop(point.id)
-        dp.distances[newClust.id] = min
-        newClust.distances[dp.id] = min
-
-
-
-print distances
+        min = float('inf')#distPoint(float("inf"), None)
+        if point1 in dp.distances:
+            dp.distances.pop(point1)
+        if point2 in dp.distances:
+            dp.distances.pop(point2)
+        dp.distances[newClust.id] = newClust.distances[dp.id]
 
 
 def findMin(distances):
-    min = (float("inf"),float("inf"),distPoint(float("inf"), None))
+    #min = (float("inf"),float("inf"),distPoint(float("inf"), None))
+    min = (float("inf"),float("inf"),float("inf"))
     for id, innerSet in distances.items():
         for i, dp in innerSet.distances.items():
-            if dp < min[2]:
-                #min = (id, dp.pnt.id, dp)
+            if dp < min[2]: # Min
                 min = (id, i, dp)
     return (min[0], min[1])
 
-smallestDistPoint = findMin(distances)
+def findMax(distances):
+    max = (-float("inf"),-float("inf"),distPoint(-float("inf"), None))
+    for id, innerSet in distances.items():
+        for i, dp in innerSet.distances.items():
+            if dp > max[2]: # Max
+                max = (id, i, dp)
+    return (max[0], max[1])
 
+def findMean(distances):
+    min = (-float("inf"),float("inf"),distPoint(-float("inf"), None))
+    sum = 0
+    for id, innerSet in distances.items():
+        for i, dp in innerSet.distances.items():
+            if dp > min[2]: # Max
+                min = (id, i, dp)
+    return (min[0], min[1])
+
+
+def getMin(dist1, dist2):
+    return 0.5*(dist1 + dist2 - abs(dist1 - dist2))
+
+def getMax(dist1, dist2):
+    return 0.5*(dist1 + dist2 + abs(dist1 - dist2))
+
+
+
+# def getMedian(n1, n2, d1, d2):
+#     ai = n1 / (n1 + n2)
+#     aj = n2 / (n1 + n2)
+#     return (ai*d1) + (aj*d2)
+
+def getMedian(dist1, dist2):
+
+    sum = 0
+    for id1, d1 in dist1.items():
+        sum += d1
+    for id2, d2 in dist2.items():
+        sum += d2
+    tot = len(dist1) + len(dist2)
+    return sum/tot
 
 def merge(point1, point2):
     d1 = distances[point1]
@@ -112,20 +151,63 @@ def merge(point1, point2):
     distances.pop(point2)
 
     newClust = cluster(d1.id + "," + d2.id)
-    newClust.points.update(d1.points)
-    newClust.points.update(d2.points)
-    updateDistances(newClust, distances)
+
+    #Remove the distances from eachother
+    d1.distances.pop(point2)
+    d2.distances.pop(point1)
+
+    for d, dist1 in d1.distances.items():
+        min = getMedian(d1.distances, d2.distances)#getMin(dist1, d2.distances[d])
+        newClust.distances[d] = min
+
+    updateDistances(point1, point2, newClust, distances) # Update the distances to every other cluster.
     distances[newClust.id] = newClust
-    print d1, d2
+
+def printDistances():
+    for d in distances.keys():
+        print d + " ",
+
+def singleLink():
+    k = 4
+    while len(distances) > k:
+        #smallestDistPoint = findMin(distances)
+        smallestDistPoint = findMin(distances)
+        merge(smallestDistPoint[0], smallestDistPoint[1])
+        print smallestDistPoint
+
+def completeLink():
+    k = 4
+    while len(distances) > k:
+        maxDistPoint = findMin(distances)
+        merge(maxDistPoint[0], maxDistPoint[1])
+        print maxDistPoint
+
+def meanLink():
+    k = 4
+    while len(distances) > k:
+        maxDistPoint = findMean(distances)
+        merge(maxDistPoint[0], maxDistPoint[1])
+        print maxDistPoint
+
+# Compute complete link clustering
+completeLink()
+
+printDistances()
 
 
-merge(smallestDistPoint[0], smallestDistPoint[1])
 
-smallestDistPoint = findMin(distances)
+pyplot.scatter(x, y)
 
-print smallestDistPoint
+fig = pyplot.figure()
+ax = fig.add_subplot(111)
 
-merge(smallestDistPoint[0], smallestDistPoint[1])
+A = x
+B = y
+C = z
 
-print distances
+pyplot.scatter(A,B)
+for xyz in zip(A, B, C):
+    ax.annotate('(%s)' % xyz[2], xy=(xyz[0], xyz[1]), textcoords='data')
 
+pyplot.grid()
+pyplot.show()
---
+++ kMedianClustering/GonzalesAlgorithm/kmeans_gonzales.py
@@ -0,0 +1,112 @@
+import math
+
+from matplotlib import pyplot
+
+
+class pnt:
+    def __init__(self, id, x, y, centerInd):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.centerInd = centerInd
+
+    def __str__(self):
+        return "(" + self.x + "," + self.y + ")"
+
+
+points = []
+centers = []
+distances = []
+
+def distance(pnt1, pnt2):
+    x1 = pnt1.x
+    x2 = pnt2.x
+    y1 = pnt1.y
+    y2 = pnt2.y
+    return math.sqrt((float(x2) - float(x1))**2 + (float(y2) - float(y1))**2)
+
+def Init2DClusters(path):
+    cluster_list = []
+    center = None
+    gotCenter = False
+    with open(path, 'r') as f:
+        for line in f:
+            l = line.split()
+            if not gotCenter:
+                center = pnt(l[0], l[1], l[2], l[0])
+                gotCenter = True
+
+            point = pnt(l[0], l[1], l[2], 1)
+            points.append(point)
+            centers.append(center)
+            dist = distance(center, point)
+            distances.append(dist)
+
+    return cluster_list
+
+clusters = Init2DClusters("/Users/jesuszarate/SchoolSemesters/Spring2017/CS6140-DataMining/Clustering/Resources/C2.txt")
+
+'''
+Choose c1 in X arbitrarily, and set phe[j] = 1 for all j in [n]
+for i = 2 to k:
+    M = 0
+    ci = x1
+    for j = 1 to n:
+        if d(xj , Cphe[j]) > M:
+            M = d(xj , Cphe[j])
+            ci = xj
+    for j = 1 to n:
+        if d(xj , Cphe[j]) > d(xj , ci):
+            phe[j] = i
+'''
+
+
+k = 3
+n = len(points)
+C = set()
+C.add(points[0].id)
+
+# Represents the centers
+for i in range(1, k):
+    M = 0
+    ci = points[0]
+
+    for j in range(0, n):
+        if distance(points[j], centers[j]) > M:
+            M = distance(points[j], centers[j])
+            ci = points[j]
+
+    for j in range(0, n):
+        if distance(points[j], centers[j]) > distance(points[j], ci):
+            centers[j] = ci
+            C.add(ci.id)
+
+
+def showClusterPlot():
+    clusterX1 = []
+    clusterY1 = []
+    clusterX2 = []
+    clusterY2 = []
+    clusterX3 = []
+    clusterY3 = []
+    for i in range(0, n):
+        if centers[i].id == '1':
+            clusterX1.append(points[i].x)
+            clusterY1.append(points[i].y)
+        elif centers[i].id == '534':
+            clusterX2.append(points[i].x)
+            clusterY2.append(points[i].y)
+        elif centers[i].id == '1004':
+            clusterX3.append(points[i].x)
+            clusterY3.append(points[i].y)
+
+    pyplot.scatter(clusterX1, clusterY1, c='red')
+    pyplot.scatter(clusterX2, clusterY2, c='blue')
+    pyplot.scatter(clusterX3, clusterY3, c='green')
+    pyplot.show()
+
+
+# def get3MeanCost():
+#
+#
+# get3MeanCost()
